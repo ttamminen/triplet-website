3af91a1699b520801249729f0e2a782d673ade49
Tatu Tamminen <ttamminen@gmail.com>
2015-01-30 17:11:30 +0200

    production configs

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'https://triplet.herokuapp.com/blog',
+        url: process.env.BLOG_URL,
         mail: {},
         database: {
             client: 'postgres',
@@ -25,7 +25,7 @@ config = {
 
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: process.env.PORT
         }
