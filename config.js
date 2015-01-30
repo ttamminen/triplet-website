792d46ad00de1b543cc1b106200c782d5dbdc006
Tatu Tamminen <ttamminen@gmail.com>
2015-01-30 16:07:18 +0200

    production url changed

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://www.triplet.fi/blog',
         mail: {},
         database: {
             client: 'postgres',
