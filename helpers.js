7e89bc516f21c8db1a20321b1190243432b03eae
Tatu Tamminen <ttamminen@gmail.com>
2015-03-24 21:30:14 +0200

    about author section

--- helpers.js
+++ helpers.js
@@ -2,6 +2,10 @@ var hbs = require('express-hbs');
 
 module.exports = function(){  
     hbs.registerHelper('blogPostMiniImage', function(imageUrl) {
+    	if(!imageUrl) {
+    		return "";
+    	}
+
     	var extension = imageUrl.slice(imageUrl.lastIndexOf('.'));
 		var fileNameWithoutExtension = imageUrl.slice(0, imageUrl.lastIndexOf('.'));
 		return fileNameWithoutExtension + "_mini" + extension;
