934d0fada53305ee9ef34eff94be704bb69c5fa2
Tatu Tamminen <ttamminen@gmail.com>
2015-07-09 12:34:34 +0300

    better build system

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,6 @@ var imagemin = require('gulp-imagemin');
 var pngcrush = require('imagemin-pngcrush');
 var minifyCSS = require('gulp-minify-css');
 
-var themeRoot = 'content/themes/triplet/assets/';
 var assetsRoot = 'assets/';
 
 gulp.task('sass', function() {
@@ -37,7 +36,7 @@ gulp.task('sass', function() {
       this.emit('end');
     })
     .pipe(postcss([ require('autoprefixer')({ browsers: ['> 1%', 'IE 9'], cascade: false }) ]))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest('public/css'))
     .pipe(livereload());
 });
 
@@ -46,7 +45,7 @@ gulp.task('sassmin', function() {
     .pipe(sass()).on('error', gutil.log)
     .pipe(postcss([ require('autoprefixer')({ browsers: ['> 1%', 'IE 9'], cascade: false }) ]))
     .pipe(minifyCSS({keepBreaks:true}))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('scripts', function () {
@@ -62,7 +61,7 @@ gulp.task('scripts', function () {
     .pipe(source('app.js'))
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(gulp.dest('./dist/js/'));
+    .pipe(gulp.dest('./public/js/'));
 });
 
 gulp.task('javascript', function () {
@@ -81,7 +80,7 @@ gulp.task('javascript', function () {
       .pipe(streamify(uglify()))
       .on('error', gutil.log)
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist/js/'));
+    .pipe(gulp.dest('./public/js/'));
 
   // "globby" replaces the normal "gulp.src" as Browserify
   // creates it's own readable stream.
@@ -114,7 +113,7 @@ gulp.task('scriptsmin', function () {
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(concat('all.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('html', function () {
@@ -123,13 +122,13 @@ gulp.task('html', function () {
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('public'))
     .pipe(livereload());
 });
 
 gulp.task('font', function () {
   return gulp.src(assetsRoot + 'fonts/**')
-    .pipe(gulp.dest('dist/fonts'));
+    .pipe(gulp.dest('public/fonts'));
 });
 
 gulp.task('image', function () {
@@ -145,68 +144,45 @@ gulp.task('image', function () {
         pngcrush()
       ]
     }))
-    .pipe(gulp.dest('dist/images'));
+    .pipe(gulp.dest('public/images'));
 });
 
 gulp.task('static', function () {
   return gulp.src(assetsRoot + 'static/**', { dot: true })
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('public'));
 });
 
 // Watch Files For Changes
-gulp.task('watch', ['createtheme'], function() {
-  livereload.listen({ basePath: 'dist' });
-  gulp.watch(assetsRoot + 'js/**/*', ['scripts', 'createtheme', 'server:restart']);
-  gulp.watch(assetsRoot + 'styles/**/*.scss', ['sass', 'createtheme', 'server:restart']);
-  gulp.watch(assetsRoot + 'html/**/*.html', ['html', 'sass', 'scripts', 'createtheme', 'server:restart']);
-  gulp.watch(assetsRoot + 'images/**/*', ['image', 'createtheme', 'server:restart']);
+gulp.task('watch', function() {
+  livereload.listen({ basePath: 'public' });
+  gulp.watch(assetsRoot + 'js/**/*', ['scripts', 'server:restart']);
+  gulp.watch(assetsRoot + 'styles/**/*.scss', ['sass', 'server:restart']);
+  gulp.watch(assetsRoot + 'html/**/*.html', ['html', 'sass', 'scripts', 'server:restart']);
+  gulp.watch(assetsRoot + 'images/**/*', ['image', 'server:restart']);
 });
 
-gulp.task('styleguide', function (next) {
-  var fileServer = require('ecstatic')({
-    root: './dist',
-    cache: 'no-cache',
-    showDir: true,
-    gzip: true,
-    defaultExt: true });
-  var port = 8000;
-  require('http').createServer()
-      .on('request', function (req, res) {
-          fileServer(req, res);
-      })
-      .listen(port, function () {
-          gutil.log('Server is listening on ' + gutil.colors.magenta('http://localhost:' + port + '/'));
-          next();
-      });
-});
-
-gulp.task('ghost', ['createtheme'], function () {
+gulp.task('site', function () {
   nodemon({
     script: 'index.js',
     env: { 'NODE_ENV': 'production' },
     ext: 'scss js',
     ignore: ['dist/**/*', 'core/**/*', 'content/**/*', 'node_modules/**/*']
   })
-  .on('change', ['sass', 'image', 'font', 'static', 'scripts', 'html', 'createtheme'])
+  .on('change', ['sass', 'image', 'font', 'static', 'scripts', 'html'])
   .on('restart', livereload);
 });
 
-gulp.task('createtheme', ['sass', 'image', 'font', 'static', 'javascript', 'html'], function () {
-  return gulp.src(['dist/css/**/*.css', 'dist/fonts/**/*.*', 'dist/images/**/*.*', 'dist/js/**/*.js'], { base: './dist'})
-    .pipe(gulp.dest(themeRoot));
-});
-
 // run server
-gulp.task( 'server:start', ['createtheme'], function() {
+gulp.task('server:start', function() {
   server.listen( { path: './index.js' } );
 });
 
 // restart server if app.js changed
-gulp.task( 'server:restart', ['createtheme'], function() {
+gulp.task('server:restart', function() {
   server.restart;
 });
 
 // Default Task
-gulp.task('default', ['sass', 'image', 'font', 'static', 'javascript', 'html', 'createtheme', 'server:start', 'styleguide', 'watch' ]);
+gulp.task('default', ['sass', 'image', 'font', 'static', 'javascript', 'html', 'server:start', 'watch' ]);
 
-gulp.task('build', ['sassmin', 'image', 'font', 'static', 'scriptsmin', 'html', 'createtheme', 'ghost']);
+gulp.task('build', ['sassmin', 'image', 'font', 'static', 'scriptsmin', 'html', 'site']);
--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 var ghost = require('ghost'),
     express = require('express'),
+    hbs  = require('express-hbs'),
     path = require('path'),
     parentApp = express();
 
-parentApp.get('/', function(req, res) {
-  res.redirect('/blog');
-});
-
+parentApp.engine('hbs', hbs.express4({
+  defaultLayout: __dirname + '/views/layouts/main.hbs'
+}));
+parentApp.set('view engine', 'hbs');
+parentApp.set('views', __dirname + '/views');
 parentApp.use(express.static(__dirname + '/public'));
 
+console.log(__dirname + '/views/layouts/main.hbs');
+
+parentApp.get('/', function (req, res) {
+  res.render('home');
+});
+
+parentApp.get('/styleguide', function (req, res) {
+  res.render('styleguide');
+});
+
 ghost({
-	config: path.join(__dirname, 'config.js')
+  config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
   parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);  
   require('./helpers')();
