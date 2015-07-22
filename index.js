12d02dbea4841ba70eb0f98be134150bd4763159
Tatu Tamminen <ttamminen@gmail.com>
2015-07-22 16:23:47 +0300

    tries to change Ghost to be a middleware

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 var ghost = require('ghost'),
     express = require('express'),
     hbs  = require('express-hbs'),
+    path = require('path'),
     parentApp = express();
 
 process.env.PWD = process.cwd();
 
-//parentApp.set('port', (process.env.PORT || 2368));
+function processBuffer( buffer, app ){
+  while( buffer.length ){
+    var request = buffer.pop();
+    app( request[0], request[1] );
+  }
+}
+
+function makeGhostMiddleware(options, cb) {
+  var requestBuffer = [];
+  var app = false;
+
+  ghost( options ).then( function( ghost ){
+    app = ghost.rootApp;
+    processBuffer( requestBuffer, app );
+    cb();
+  });
+
+  return function handleRequest(req, res){
+    if(!app) {
+      requestBuffer.unshift( [req, res] );
+    } else {
+      app( req, res );
+    }
+  };
+}
+
+parentApp.set('port', (process.env.PORT || 2368));
 
 parentApp.engine('hbs', hbs.express4({
-  defaultLayout: process.env.PWD + '/views/layouts/main.hbs'
+  defaultLayout: path.join(process.cwd(), 'views', 'layouts', 'main.hbs')
 }));
 parentApp.set('view engine', 'hbs');
-parentApp.set('views', process.env.PWD + '/views');
-parentApp.use(express.static(process.env.PWD + '/public'));
+parentApp.set('views', path.join(process.cwd(), 'views'));
+parentApp.use(express.static(path.join(process.cwd(), 'public')));
 
 parentApp.get('/', function (req, res) {
   var description = 'TripleT Softworks - Web Development Consulting';
@@ -26,14 +53,16 @@ parentApp.get('/styleguide', function (req, res) {
   res.render('styleguide');
 });
 
+require('./helpers')();
+
+parentApp.use( '/blog', makeGhostMiddleware({
+  config: path.join(process.cwd(), 'config.js')
+}, function (ghostServer) {
+  //parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+  require('./helpers')();
+  //ghostServer.start(parentApp);
+}));
+
 parentApp.listen(parentApp.get('port'), function() {
   console.log('Node app is running on port', parentApp.get('port'));
-});
-
-ghost({
-  config: process.env.PWD + '/config.js'
-}).then(function (ghostServer) {
-  parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
-  require('./helpers')();
-  ghostServer.start(parentApp);
 });
